280391f6ea78322c6384a748d7eb29c19abef556
cgfennelly <c.g.fennelly@gmail.com>
2021-11-29 14:00:00 +0000

    additional attempt to resolve Netlify issue

--- src/components/ArticleVote.js
+++ src/components/ArticleVote.js
@@ -1,4 +1,4 @@
-import { useState } from "react/cjs/react.development";
+import { useState } from "react";
 import { addVoteArticle } from "../api";
 
 const ArticleVote = ({ article_id, votes }) => {
